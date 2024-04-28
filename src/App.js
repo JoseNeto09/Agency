9f2bc2d3da86c04ece6eb69a4ae2f5481eeb4b34
José Neto <joselopessobrinhoneto9@gmail.com>
2024-04-28 13:43:11 -0300

    services

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import './App.css';
 
 import Header from './components/Header/Header';
 import Hero from './components/UI/Hero';
+import Counter from './components/UI/Counter';
+import Services from './components/UI/Services';
+
 function App() {
   return (
     <>
       <Header />
-      <Hero/>
+      <Hero />
+      <Counter />
+      <Services />
     </>
   );
 }
